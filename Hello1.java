75fd22ff214b3c4118703f48587972fdcc3b2872
SUREKASUBA <surekasuba97@gmail.com>
2017-06-20 16:56:44 +0530

    Create Hello1.java

---
+++ Hello1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class hello1
+{
+ public static void main(String [] args)
+ {
+	 int i;
+  for(i=0;i<=5;i++)
+  {
+  System.out.println("Hello");
+  }
+  
+ }
+} 
