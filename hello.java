1a65be284ab4907acb2ef0152dc55f91ea3dce9c
SUREKASUBA <surekasuba97@gmail.com>
2017-06-20 15:49:31 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class hello
+{
+ public static void main(String [] args)
+ {
+  if(System.out.printf("Hello World")==null)
+  {
+	  
+  }
+ }
+} 
