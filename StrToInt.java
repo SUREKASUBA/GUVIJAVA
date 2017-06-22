130dcfbd5e80fd127dfcbed6f010bc93f0bc25e8
SUREKASUBA <surekasuba97@gmail.com>
2017-06-22 11:53:37 +0530

    Create StrToInt.java

---
+++ StrToInt.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+class strint
+{
+   public static void main(String args[])
+   {
+      int n;
+	  String s;
+      Scanner in = new Scanner(System.in);
+ 
+      System.out.println("Enter a String");
+      s=in.nextLine();
+	  System.out.println("STRING  IS:"+s);
+      n=Integer.parseInt(s);
+ 
+      System.out.println("INTEGER IS:"+n);
+   }
+}
